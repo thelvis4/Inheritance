4e036ddc495b41a907d8c9e4168c0127c14058b5
Andrei Raifura <thelvis4@gmail.com>
2015-08-16 15:50:24 +0300

    Create Car class

---
+++ CodeWăy_Inheritance/Classes/Cars/Car.swift
@@ -0,0 +1,43 @@
+//
+//  Car.swift
+//  CodeWăy_Inheritance
+//
+//  Created by Andrei Raifura on 8/16/15.
+//  Copyright © 2015 YOPESO. All rights reserved.
+//
+
+class Car {
+    let frontLeft: Wheel
+    let frontRight: Wheel
+    let rearLeft: Wheel
+    let rearRight: Wheel
+    
+    let steering: Steering
+    let driving: Driving
+    let manufacturer: Manufacturer
+    
+    init(frontLeft: Wheel, frontRight: Wheel, rearLeft: Wheel, rearRight: Wheel,
+        steering: Steering, driving: Driving, manufacturer: Manufacturer) {
+            self.frontLeft = frontLeft
+            self.frontRight = frontRight
+            self.rearLeft = rearLeft
+            self.rearRight = rearRight
+            
+            self.steering = steering
+            self.driving = driving
+            self.manufacturer = manufacturer
+    }
+    
+    func turnLeft(degrees: Double) {
+        steering.turnLeft(degrees)
+    }
+    
+    func turnRight(degrees: Double) {
+        steering.turnRight(degrees)
+
+    }
+    
+    func accelerate(kph: Double) {
+        driving.accelerate(kph)
+    }
+}
