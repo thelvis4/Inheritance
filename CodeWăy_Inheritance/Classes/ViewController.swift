276b8c58dc6aa6cdd545811bc2a0739a53cbe068
Andrei Raifura <thelvis4@gmail.com>
2015-08-16 10:49:44 +0300

    Create a new iOS project

---
+++ CodeWăy_Inheritance/Classes/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  CodeWăy_Inheritance
+//
+//  Created by Andrei Raifura on 8/16/15.
+//  Copyright © 2015 YOPESO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
